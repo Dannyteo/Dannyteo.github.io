888ae5a9fb1a8fe63d80d770b1f3da37ca49c3ec
Danny Teo <teo_yongsong@yahoo.com.sg>
2021-10-29 17:12:36 +0800

    Update app.js

--- projects/pokemon/app.js
+++ projects/pokemon/app.js
@@ -1,20 +1,3 @@
-// const btn = document.querySelector('#v2');
-// btn.onclick = function () {
-//     console.log('You clicked me!');
-//     console.log('I hope it works!');
-// }
-
-// function scream() {
-//     console.log('aaaaaaaaaaahhhh!!!');
-//     console.log('Stop it!!!');
-// }
-
-// btn.onmouseenter = scream;
-// const btn3 = document.querySelector('#v3');
-// btn3.addEventListener('dblclick', function () {
-//     alert('Clicked!');
-// })
-
 const conatiner = document.querySelector('#container');
 const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 

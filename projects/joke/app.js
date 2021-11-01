3a9cfb9706315fd193328eacb912c489f9f06bb7
Danny Teo <teo_yongsong@yahoo.com.sg>
2021-11-01 17:15:07 +0800

    Update app.js

--- projects/joke/app.js
+++ projects/joke/app.js
@@ -1,11 +1,24 @@
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay)
-    })
-}
+// const delayedColorChange = (color, delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay)
+//     })
+// }
+
+async function rainbow() {
+     await delayedColorChange('red', 2000)
+     await delayedColorChange('orange', 2000)
+     await delayedColorChange('yellow', 2000)
+     await delayedColorChange('green', 2000)
+     await delayedColorChange('blue', 2000)
+     await delayedColorChange('indigo', 2000)
+     await delayedColorChange('violet', 2000)
+     return ('All done!')
+ }
+
+ rainbow().then(() => console.log('End of rainbow'))
 
 const fetchBitcoinPrice = async () => {
     try {

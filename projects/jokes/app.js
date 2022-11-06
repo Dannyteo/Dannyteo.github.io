45b2a6b858e676c85640d973d5bbe2af4e745b55
Danny Teo <teo_yongsong@yahoo.com.sg>
2022-11-06 18:07:32 +0800

    Update app.js

--- projects/jokes/app.js
+++ projects/jokes/app.js
@@ -3,7 +3,7 @@ const button = document.querySelector('button');
 
 const addNewJoke = async () => {
     const jokeText = await getDadJoke();
-    //console.log(jokeText);
+    console.log(jokeText);
     const newLI = document.createElement('LI');
     newLI.append(jokeText);
     jokes.append(newLI);
@@ -13,7 +13,7 @@ const getDadJoke = async () => {
     try {
         const config = { headers: { accept: 'application/json' } };
         const res = await axios.get('https://icanhazdadjoke.com/', config);
-        //console.log(res.data.joke);
+        console.log(res.data.joke);
         return res.data.joke;
     } catch (e) {
         return "No joke available. Sorry!"

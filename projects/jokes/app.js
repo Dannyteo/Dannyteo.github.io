c010dfb83d0bb0824c0fd0c1fab652ec94a81042
Danny Teo <teo_yongsong@yahoo.com.sg>
2022-11-06 22:41:53 +0800

    Update app.js

--- projects/jokes/app.js
+++ projects/jokes/app.js
@@ -12,7 +12,7 @@ const addNewJoke = async () => {
 
 const getDadJoke = async () => {
 	try{
-		const config = { headers: {"Content-Type": "application/json", "Accept": "application/json}
+		const config = { headers: {"Content-Type": "application/json", "Accept": "application/json"}
 		const res = await axios.get(`https://icanhazdadjoke.com/`, config);
         	return res.data.joke;
 	}catch (e) {
